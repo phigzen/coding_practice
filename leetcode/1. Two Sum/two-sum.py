110ca638085c84d5cb885a44b13f8fd9594827e0
zhangqibot <zhangqibot@foxmail.com>
2020-05-02 21:44:43 +0800

    nice solution

--- leetcode/1. Two Sum/two-sum.py
+++ leetcode/1. Two Sum/two-sum.py
@@ -38,3 +38,12 @@ class Solution2:
             if target - x in nums[i + 1:]:
                 return [i, nums.index(target - x, i + 1)]
         return []
+
+
+class Solution3:
+    def twoSum(self, nums, target):
+        hash_map = dict()
+        for i, x in enumerate(nums):
+            if target - x in hash_map:
+                return [i, hash_map[target - x]]
+            hash_map[x] = i
